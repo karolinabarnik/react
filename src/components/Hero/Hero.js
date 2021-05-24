97195ee302d7dd91907e2faadc68e94416c5ab5d
Karolina Barnik <karolinabarnik@gmail.com>
2021-05-24 22:53:43 +0200

    add hero.js

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+
+
+const Hero = props => (
+  <header className={styles.component}>
+    <h2 className={styles.title}>{props.titleText}</h2>
+    <img src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" className={styles.image}></img>
+  </header>
+);
+
+
+export default Hero;
