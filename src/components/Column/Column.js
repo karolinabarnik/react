1d6ed47d9b38f8cca344fa143f3f4830ccd4f3ee
Karolina Barnik <karolinabarnik@gmail.com>
2021-05-26 11:11:06 +0200

    Add card cicon

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,9 +1,43 @@
+import React from 'react';
+import styles from './Column.scss';
+import PropTypes from 'prop-types';
+import Card from '../Card/Card';
+import { settings } from '../../data/dataStore';
+import Creator from '../Creator/Creator';
+import Icon from '../Icon/Icon';
 
+class Column extends React.Component {
+  
+  static propTypes = {
+    title: PropTypes.node,
+    cards: PropTypes.array,
+    icon: PropTypes.node,
+    addCard: PropTypes.func,
+  }
 
-const Column = props => (
-    <section className={styles.component}>
-      <h3 className={styles.title}></h3>
-    </section>
-  );
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
+  }
+ 
+  render() {
+    const {title, icon, cards, addCard} = this.props;
+    
+    return (
+      <section className={styles.component}>
+        <h3 className={styles.title}>{title}<span className={styles.icon}><Icon name={icon}></Icon></span></h3>
 
-  export default Column;
+        <div className={styles.cards}>
+          {cards.map(cardData => (
+            <Card key={cardData.id} {...cardData} />
+          ))}
+        </div>
+
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={addCard}/>
+        </div>
+      </section>
+    );
+  }
+}
+    
+export default Column;
