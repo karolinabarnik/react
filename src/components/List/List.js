5eaea9c7a8e55cb40a838fdbb259edd5e7a6eada
Karolina Barnik <karolinabarnik@gmail.com>
2021-05-26 10:51:26 +0200

    Add Creator Icon Button

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,32 +6,64 @@ import PropTypes from 'prop-types';
 import columnStyles from '../Column/Column.scss'
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
+import Creator from '../Creator/Creator.js';
 
 
 class List extends React.Component {
+    state = {
+        columns: this.props.columns || [],
+      }
+
     static propTypes = {
         title: PropTypes.node.isRequired,
         description: PropTypes.node,
         columns: PropTypes.array,
+        image: PropTypes.node,
+        addColumn: PropTypes.func,
       }
+
     static defaultProps = {
         description: settings.defaultListDescription,
     }  
+
+    addColumn(title){
+        this.setState(state => (
+      {
+      columns: [
+              ...state.columns,
+              {
+                key: state.columns.length ? state.columns[state.columns.length
+                title,
+                icon: 'list-alt',
+                cards: []
+      } ]
+      } ));
+      }
+
     render() {
-    return (
-      <section className={styles.component}>
-        <Hero titleText={this.props.title}
-         imageHero={this.props.sourceImg} />
-        <div className={styles.description}>
-            {ReactHtmlParser(this.props.description)}
-        </div>
-        <div className={styles.columns}>
-            <column className={columnStyles.component}><h4>Animals</h4></column>
-            <column className={columnStyles.component}><h4>Plants</h4></column>
-            <column className={columnStyles.component}><h4>Minerals</h4></column>
-        </div>
-      </section>
-    )
+        const {title, image, description, columns, addColumn} = this.props;
+
+        return (
+            <section className={styles.component}>
+            
+            <Hero titleText={this.props.title}
+            imageHero={this.props.sourceImg} />
+                <div className={styles.description}>
+                    {ReactHtmlParser(this.props.description)}
+                </div>
+            
+            <div className={styles.columns}>
+            {columns.map(columnData => (
+            <   Column key={columnData.id} {...columnData} />
+            ))}
+            </div>
+
+            <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={addColumn}/>
+            </div>
+
+            </section>
+    );
   }
 }
 
