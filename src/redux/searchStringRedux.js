784e003bd3388e9f134d88142574eabc4674e609
Karolina Barnik <karolinabarnik@gmail.com>
2021-06-03 23:35:30 +0200

    Search

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,16 +1,18 @@
 // selectors
-export const getSearchString = ({ searchString }) => searchString;
-export const countAllCards = ({cards}) => cards.lenght;
-export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title));
+export const getSearchString = state => state.searchString;
+export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
+export const countAllCards = ({cards}) => cards.length;
 
 // action name creator
-
 const reducerName = 'searchString';
 const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
+export const SEARCH_STRING = createActionName('SEARCH_STRING');
 
 // action creators
+export const createAction_changeSearchString = payload => ({ payload: payload, id: shortid.generate() , type: SEARCH_STRING });
+
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,8 @@ import {combineReducers, createStore} from 'redux';
 import initialStoreData from '../data/dataStore';
 import columnsRedux from './columnsRedux'
 import cardsRedux from './cardsRedux'
+import searchStringReducer from './searchStringRedux';
+
 
 // define initial state and shallow-merge initial data
 const initialState = {
@@ -9,12 +11,15 @@ const initialState = {
   lists: initialStoreData.lists,
   columns: initialStoreData.columns,
   cards: initialStoreData.cards,
+  searchString: 'ter',
 };
 
 // define reducers
 const reducers = {
     columns: columnsRedux,
-    cards: cardsRedux
+    cards: cardsRedux,
+    searchString: searchStringReducer,
+
 };
 
 // add blank reducers for initial state properties without reducers
