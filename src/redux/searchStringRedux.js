9f058cc940b907661ffe37e8b77549937f1226f5
Karolina Barnik <karolinabarnik@gmail.com>
2021-06-04 20:57:31 +0200

    Search

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,4 +1,7 @@
+import shortid from 'shortid';
+
 // selectors
+
 export const getSearchString = state => state.searchString;
 export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
 export const countAllCards = ({cards}) => cards.length;
